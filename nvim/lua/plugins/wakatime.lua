9cda039cc5022204dd6fd8bc595820434bb7370d
Joseph Simeon <josephdmsimeon@icloud.com>
2025-06-20 12:12:59 +1000

    Added plugin wakatime

---
+++ nvim/lua/plugins/wakatime.lua
@@ -0,0 +1,14 @@
+--  @file   wakatime.lua
+--  @brief  Added the wakatime plugin.
+--  @author Joseph Simeon
+--  @date   20-06-2025 12:05:14
+--
+--  @note Check website to get API key, call :WakaTimeApiKey and paste in the key.
+
+return {
+    'wakatime/vim-wakatime', 
+    lazy = false,
+    setup = function ()
+        vim.cmd([[packadd wakatime/vim-wakatime]])
+    end
+}
