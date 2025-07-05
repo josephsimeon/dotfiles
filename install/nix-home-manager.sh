71889721fed257181254202713a9eae0ab897cb5
Joseph Simeon <josephdmsimeon@icloud.com>
2025-07-06 05:00:48 +1000

    Added files for nix home-manager used on ubuntu wsl

--- install/nix-home-manager.sh
+++ install/nix-home-manager.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+mkdir -p $HOME/.config/nix
+cp $DIR/nix/* $HOME/.config/nix
+
+mkdir -p $HOME/.config/home-manager
+cp $DIR/home-manager/* $HOME/.config/home-manager
+
