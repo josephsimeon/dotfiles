0099846907683b1538f87ce54a8c6b5ccb6b2363
Joseph Simeon <josephdmsimeon@icloud.com>
2025-07-05 12:45:27 +1000

    Added in the nix.conf to the nix-darwin.sh

--- nix-darwin.sh
+++ nix-darwin.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
+
+mkdir -p $HOME/.config/nix
+cp $DIR/nix/* $HOME/.config/nix
+
 mkdir -p $HOME/.config/nix-darwin
 cp $DIR/nix-darwin/* $HOME/.config/nix-darwin
