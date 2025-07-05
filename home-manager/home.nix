eadac4cfb87ae2b32aa3c1604671498af730eda6
Joseph Simeon <josephdmsimeon@icloud.com>
2025-07-06 05:15:27 +1000

    Removed pkgs.cargo due to error

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -21,7 +21,6 @@
     pkgs.neovim
     pkgs.git
     pkgs.rustup # `rustup install stable && rustup default stable && rustup update`
-    pkgs.cargo
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
