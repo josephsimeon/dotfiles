6e9507d946d7c9ee6741266a978aa9ca763ee884
Joseph Simeon <josephdmsimeon@icloud.com>
2026-06-02 01:05:15 +1000

    Added in markdown render preview for lazy in neovim

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+--  @file   render-markdown.lua
+--  @brief  Renders markdown in buffer
+--  @author Joseph Simeon
+--  @date   02-06-2026 01:01:30
+
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },         -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
